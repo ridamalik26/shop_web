5ce06deb03dc5e6ac93de52ee8ef5fbe652cb191
ridamalik26 <malikrida406@gmail.com>
2025-12-15 02:57:57 +0500

    banner page UI

--- lib/views/side_bar_screen/upload_banner_screen.dart
+++ lib/views/side_bar_screen/upload_banner_screen.dart
@@ -1,15 +1,87 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
 
-class UploadBannerScreen extends StatelessWidget {
-  static const String id = '\BannersScreen';
+class UploadBannerScreen extends StatefulWidget {
+  static const String id = '/BannersScreen';
   const UploadBannerScreen({super.key});
 
   @override
+  State<UploadBannerScreen> createState() => _UploadBannerScreenState();
+}
+
+class _UploadBannerScreenState extends State<UploadBannerScreen> {
+  dynamic _image;
+
+  pickImage() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: false,
+    );
+
+    if (result != null) {
+      setState(() {
+        _image = result.files.first.bytes;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-          "Upload Banner Screen"
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            alignment: Alignment.topLeft,
+            child: const Text(
+              "Banners",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 36,
+              ),
+            ),
+          ),
+        ),
+
+        const Divider(
+          color: Colors.grey,
+          thickness: 2,
+        ),
+
+        Row(
+          children: [
+            Container(
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Center(
+                child: _image != null
+                    ? Image.memory(_image)
+                    : const Text('Category image'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(onPressed: (){}, child: Text('Save'),),
+            ),
+          ],
+        ),
+
+        const SizedBox(height: 10),
+
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+            onPressed: pickImage,
+            child: const Text("Pick Image"),
+          ),
+        ),
+      ],
     );
   }
 }
