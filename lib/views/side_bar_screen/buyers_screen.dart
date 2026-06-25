28642614c7d828e7349c483bd8ad7a1b7153773b
ridamalik26 <malikrida406@gmail.com>
2026-06-25 22:42:04 +0500

    buyers's header

--- lib/views/side_bar_screen/buyers_screen.dart
+++ lib/views/side_bar_screen/buyers_screen.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class BuyersScreen extends StatelessWidget {
   static const String id = '\buyersScreen';
@@ -6,9 +7,59 @@ class BuyersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Buyers Screen"
+    Widget _rowHeader(int flex,String text){
+      return Expanded(
+          flex: flex,
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.grey.shade700
+              ),
+              color: const Color(0xFF3C55EF,),
+            ),
+            child: Padding(
+                padding: const EdgeInsets.all(8),
+              child: Text(
+                text,
+                style: GoogleFonts.montserrat(
+                color: Colors.white,
+                fontSize: 15,
+                fontWeight: FontWeight.bold
+              ),
+              ),
+            ),
+          )
+      );
+    }
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Manage Buyers', style: GoogleFonts.montserrat(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+            ),
+           const SizedBox(
+             height: 15,
+           ),
+            Row(
+              children: [
+                _rowHeader(1, 'Image'),
+                _rowHeader(3, 'Full Name'),
+                _rowHeader(2, 'Email'),
+                _rowHeader(2, 'Address'),
+                _rowHeader(1, 'Delete'),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
